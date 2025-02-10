17a79107177b042e884ab21940dfb62bdb9a1381
m-umar-j <shikaslad199@gmail.com>
2025-02-10 12:51:29 +0530

    final commit

--- app.py
+++ app.py
@@ -13,19 +13,18 @@ model=ChatMistralAI(
 
 
 with gr.Blocks(css=".gradio-container {background: url('image.png') !important;}") as Iface:
-    gr.Markdown("<h1>Find Shayari</h1>")
-    
+    gr.Markdown("<h1>Search Shayari</h1>")
     with gr.Row():
         with gr.Column(scale=1):
             query = gr.Textbox(label="Which type of shayari do you want to find?")
             submit_button = gr.Button("Search")
         
         with gr.Column(scale=2):
-            chatbot = gr.Chatbot(label="Matching results", height=700)      
+            chatbot = gr.Chatbot(label="Matching results", height=600)      
     submit_button.click(
         fn=llm,
         inputs=[query, chatbot],
         outputs=[chatbot, chatbot]
 
     )
-Iface.launch()
+Iface.launch(share=True)
--- utilis.py
+++ utilis.py
@@ -5,6 +5,10 @@ from pinecone import Pinecone
 from langchain_together import TogetherEmbeddings
 from langchain_mistralai import ChatMistralAI
 import cohere
+from langchain.retrievers.contextual_compression import ContextualCompressionRetriever
+from langchain_cohere import CohereRerank
+
+
 load_dotenv()
 PINECONE_API_KEY=os.getenv("PINECONE_API_KEY")
 MISTRAL_API_KEY=os.getenv("MISTRAL_API_KEY")
@@ -29,31 +33,32 @@ index_name = "together"
 index = pc.Index(index_name)
 
 vector_store = PineconeVectorStore(index=index, embedding=embeddings)
+retriever = vector_store.as_retriever()
+
+compressor = CohereRerank(model="rerank-v3.5")
+compression_retriever = ContextualCompressionRetriever(
+    base_compressor=compressor, base_retriever=retriever
+)
 
 def retrieve(query):
-    print(1)
-    docs=vector_store.similarity_search(query, k=4)
-    print(f"length of docs is {len(docs)}")
-    results = co.rerank(query=query, documents=docs, top_n=3, model='rerank-v3.5')
+    compressed_docs = compression_retriever.invoke(query)
     
-    return results
+    return compressed_docs[0]
 
 def llm(query, history):
-    print(3)
     query=model.invoke(f"You are a helpful AI assistant. Translate the following query to urdu if the query is in urdu then return the same query. Query: {query}. Remember just translate the query and nothing else.").content
     results=retrieve(query)
     prompt=f""""
     You are a helpful AI Assistant and your job is to help user find relevant shayari.
     The query is {query}
-    The matching result after semantic search is {results[0].page_content}. You may exclude interpretation given in the results. Just respond from the context.
+    The matching result after semantic search is {results.page_content}. You may exclude interpretation given in the results. Just respond from the context.
     """
 
     try:
         response=model.invoke(prompt).content
         history = history or []
-        history.append(("User", query))
-        history.append(("Bot", results[0].page_content))
-        print(4)
+        history.append(("You", query))
+        history.append(("Assistant", results.page_content))
         return history, history
     except Exception as e:
         return history, history + [("Bot", f"Error in API request: {str(e)}")]
